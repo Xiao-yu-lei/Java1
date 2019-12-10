8379761939076f319d1f8ab95574ed915ef08407
Xiao-yu-lei <55052691+Xiao-yu-lei@users.noreply.github.com>
2019-12-10 18:27:30 +0800

    Add files via upload

---
+++ src/allwork/第十三、十四周作业/HashMap/AccountSystem.java
@@ -0,0 +1,16 @@
+
+public class AccountStystem {
+
+	public static void main(String[] args) {
+		//实例化一个电信用户类User
+		User user = new User("13800138000");
+		
+		//生成通话记录
+		User.generateCommunicateRecord();
+		
+		
+		//打印通话详单
+	    User.printDetails();
+	}
+
+}
